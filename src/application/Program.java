54c543d25e679c853bc39d9dcd9ae9a04707511c
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2023-06-28 18:09:23 +0200

    created position and changeTurn methods

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-
-		final int ROWS = 3;
-		final int COLS = 3;
-		int position;
 		
+		Scanner sc = new Scanner(System.in);
+
+//		char p1 = 'X';
+//		char p2 = 'O';
+		char turn;
+		int position;
+		Boolean game = true;
+		
+		//gameBoard' Design
 		char[][] gameBoard = {
 				{' ', '|', ' ', '|', ' '},
 				{'-', '+', '-', '+', '-'},
@@ -16,14 +23,21 @@ public class Program {
 				{' ', '|', ' ', '|', ' '}
 		};
 			
-			printGameBoard(gameBoard);
+
+		//Game
+			System.out.print("Select who will play first: ");
+			turn = sc.next().charAt(0);
 			
-			for (int i = 1; i <= ROWS; i++) {
-				for (int j = 1; j <= COLS; j++) {
-					gameBoard[i][j] = 'X';
-				}
+			while(game) {
+				printGameBoard(gameBoard);
+				System.out.println(turn + " select a position: (1-9)");
+				position = sc.nextInt();
+				position(position, gameBoard, turn);
+				printGameBoard(gameBoard);
+				game = false;
 			}
 			
+			sc.close();
 	}
 	
 	public static void printGameBoard(char[][] gameBoard) {
@@ -34,5 +48,54 @@ public class Program {
 			System.out.println();
 		}
 	}
+	
+	public static void changeTurn(char turn) {
+		if (turn == 'X') {
+			turn = 'O';
+		} else {
+			turn = 'X';
+		}
+		System.out.println("Turn: " + turn);
+	}
 
+	public static void position(int pos, char[][] gb, char turn) {
+		switch(pos) {
+		case 1:
+			gb[0][0] = turn;
+			changeTurn(turn);
+		break;
+		case 2:
+			gb[0][2] = turn;
+			changeTurn(turn);
+		break;
+		case 3:
+			gb[0][4] = turn;
+			changeTurn(turn);
+		break;
+		case 4:
+			gb[2][0] = turn;
+			changeTurn(turn);
+		break;
+		case 5:
+			gb[2][2] = turn;
+			changeTurn(turn);
+		break;
+		case 6:
+			gb[2][4] = turn;
+			changeTurn(turn);
+		break;
+		case 7:
+			gb[4][0] = turn;
+			changeTurn(turn);
+		break;
+		case 8:
+			gb[4][2] = turn;
+			changeTurn(turn);
+		break;
+		case 9:
+			gb[4][4] = turn;
+			changeTurn(turn);
+		break;
+		}
+	}
 }
